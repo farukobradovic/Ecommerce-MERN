c9a2e2bda736919d36a8377511c8e1aaaf740da1
farukobradovic <faruk_95@msn.com>
2021-01-20 16:17:24 +0100

    Order details

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems } from "../controllers/orderController.js";
+import { addOrderItems, getOrderById } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems);
+router.route("/:id").get(protect, getOrderById);
+//Ruta sa Id treba uvijek da je posljednja
 
 export default router;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,7 +12,10 @@ import {
   userRegisterReducer,
   userUpdateProfileReducer,
 } from "./reducers/userReducers";
-import { orderCreateReduceer } from "./reducers/orderReducers";
+import {
+  orderCreateReduceer,
+  orderDetailsReduceer,
+} from "./reducers/orderReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -23,6 +26,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReduceer,
+  orderDetails: orderDetailsReduceer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
